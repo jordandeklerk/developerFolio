f78da451850e4e08a0b77d47422cba6382d0e463
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2024-06-17 23:45:52 -0400

    add new animations

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,7 +25,7 @@ import jsIcon from "./assets/images/javascript.svg"; // Import the JS SVG icon
 const splashScreen = {
   enabled: true, // set false to disable splash screen
   animation: splashAnimation,
-  duration: 4190  // Set animation duration as per your animation
+  duration: 2600  // Set animation duration as per your animation
 };
 
 // Summary And Greeting Section
